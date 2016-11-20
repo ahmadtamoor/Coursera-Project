a439efda3b300194c754f8298555e02ea40fae03
ahmadtamoor <ahmadtamoor@hotmail.com>
2016-11-20 21:30:33 +0500

    added      module2-solution added      assignment2 added      assignment2/angular.min.js added      assignment2/app.js added      assignment2/fonts added      assignment2/fonts/glyphicons-halflings-regular.eot added      assignment2/fonts/glyphicons-halflings-regular.svg added      assignment2/fonts/glyphicons-halflings-regular.ttf added      assignment2/fonts/glyphicons-halflings-regular.woff added      assignment2/fonts/glyphicons-halflings-regular.woff2 added      assignment2/index.html added      assignment2/styles added      assignment2/styles/bootstrap.min.css

---
+++ module2-solution/assignment2/app.js
@@ -0,0 +1,45 @@
+(function () {
+    'use strict';
+
+
+    var shoppingList = [
+  {
+      name: "Milk",
+      quantity: "2"
+  },
+  {
+      name: "Donuts",
+      quantity: "200"
+  },
+  {
+      name: "Cookies",
+      quantity: "300"
+  },
+  {
+      name: "Chocolate",
+      quantity: "5"
+  },
+  {
+      name: "Bread",
+      quantity: "10"
+  }
+];
+
+    angular.module('ShoppingListApp', [])
+.controller('ShoppingListController', ShoppingListController);
+
+    ShoppingListController.$inject = ['$scope'];
+    function ShoppingListController($scope) {
+        $scope.shoppingList = shoppingList;
+        $scope.boughtitemList = []; 
+        
+       
+        $scope.addtoBoghtlist = function (item) {
+
+            var index = $scope.shoppingList.indexOf(item);
+            $scope.shoppingList.splice(index, 1);
+            $scope.boughtitemList.push(item);
+        };
+    }
+
+})();
